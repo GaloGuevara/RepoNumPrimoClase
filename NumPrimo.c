7d92c9d48465400b87999004c59f6008b5ab7e3b
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-10-04 16:02:50 -0500

    Creación del sistema ingreso/confirmación del número entero

--- NumPrimo.c
+++ NumPrimo.c
@@ -1,7 +1,11 @@
+//Como actividad, vamos a construir un programa en lenguaje C que calcule si un número entero ingresado por el usuario es un número primo
 #include <stdio.h>
 
 int main()
 {
-    //Como actividad, vamos a construir un programa en lenguaje C que calcule si un número entero ingresado por el usuario es un número primo
+    int num;
+    printf("Ingrese el númer entero por favor: ");
+    scanf("%d", &num);
+    printf("El numero ingresado es: %d\n", num);
     return 0;
 }
