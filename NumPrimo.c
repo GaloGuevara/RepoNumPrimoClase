65cc708db9b9febdbd6c5b070796f3f7f00b03ef
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-10-04 16:26:45 -0500

    Creación del sistema de verificación de si el numero es primo o no

--- NumPrimo.c
+++ NumPrimo.c
@@ -1,11 +1,36 @@
 //Como actividad, vamos a construir un programa en lenguaje C que calcule si un número entero ingresado por el usuario es un número primo
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
-    int num;
-    printf("Ingrese el númer entero por favor: ");
+    int num; 
+    
+    
+    printf("Ingrese el número entero por favor: ");
     scanf("%d", &num);
     printf("El numero ingresado es: %d\n", num);
+
+    
+    if (num <= 1)
+    {
+        printf("Los numeros menores o iguales a 1 no son primos");
+    } else {
+        bool esPrimo=true; 
+
+        for (int i = 2; i <= num/2; i++) 
+        {
+            if (num%1==0)
+            {
+                esPrimo=false; 
+                break;
+            }
+            
+        }
+        if(esPrimo) 
+            printf("%d es primo\n", num);
+        else 
+            printf("%d no es primo\n", num);
+    }
     return 0;
 }
