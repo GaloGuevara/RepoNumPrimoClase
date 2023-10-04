1eb3e42e118f3772b04ea9b074f7fc84d0964b71
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-10-04 16:00:32 -0500

    Creación de la estructura del codigo con la actividad a realizar

---
+++ NumPrimo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    //Como actividad, vamos a construir un programa en lenguaje C que calcule si un número entero ingresado por el usuario es un número primo
+    return 0;
+}
